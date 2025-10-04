fb70912eeddf8b327005a8722f4e9130cfdfc1b8
Data-0 <111650164+Data-0@users.noreply.github.com>
2025-10-04 14:00:44 +0200

    Add files via upload

---
+++ RuinedFatDestroyer.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppSystem.Buffers;
+using Il2CppTLD.IntBackedUnit;
+using MelonLoader;
+using SharpArrows;
+
+namespace FatDestroyer
+{
+
+    public class DespawnFat : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg(System.ConsoleColor.White, "Fat Destroyer Started");
+            Settings.instance.AddToModSettings("Fat Destroyer");
+        }
+
+        [HarmonyPatch(typeof(GearItem), nameof(GearItem.Deserialize))]
+        public class GearItem_Awake
+        {
+            public static void Postfix(ref GearItem __instance)
+            {
+                if (__instance == null) return;
+                if (string.IsNullOrWhiteSpace(__instance.name)) return;
+
+                float hp = __instance.GetNormalizedCondition() * 100;
+                float limit = Settings.instance.minAmount;
+
+                if ( __instance.name.StartsWith("GEAR_AnimalFat") && hp<=limit)
+                {    
+                                          
+                    Melon<DespawnFat>.Logger.Msg("Despawned RUINED GEAR_AnimalFat");
+                    GameManager.DestroyObject(__instance);
+                    
+                }
+
+
+            }
+        }
+    }
+
+}
---
+++ Settings.cs
@@ -0,0 +1,20 @@
+﻿using Il2CppNewtonsoft.Json.Linq;
+using ModSettings;
+using System.ComponentModel;
+
+namespace SharpArrows
+{
+    internal class Settings : JsonModSettings
+    {
+        internal static Settings instance = new Settings();
+
+        [Section("Despawn Fat")]        
+
+        [Name("What % to despawn fat at")]
+        [Slider(10f, 80f)]
+        public int minAmount = 20;        
+
+    }
+}
+
+
